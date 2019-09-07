d34d93abbd3233a0a5b508e33af6b5d55f9eaabd
Devin Arnold <dbarnold321@gmail.com>
2019-09-07 11:51:52 -0600

    added db's

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+# config.hosts << "2e65dbfc.ngrok.io"
+Rails.application.configure do
+    config.hosts << "2e65dbfc.ngrok.io"
+    config.hosts.clear
+  end
